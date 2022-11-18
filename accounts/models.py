1d7ed9bcee574ebdafe8fb0f2ba09038c9b6af64
jaejae200 <kj31049@naver.com>
2022-11-18 14:50:03 +0900

    feat : login, logout 기능

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-from imagekit.models import ProcessedImageField
-from imagekit.processors import Thumbnail
 
 # Create your models here.
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
-from django.contrib.auth import login
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -10,7 +12,7 @@ def signup(request):
         signup_form = CustomUserCreationForm(request.POST)
         if signup_form.is_valid():
             user = signup_form.save()
-            login(request, user)
+            auth_login(request, user)
             return redirect('articles:index')
     else:
         signup_form = CustomUserCreationForm()
@@ -20,3 +22,28 @@ def signup(request):
     }
 
     return render(request, 'accounts/signup.html', context)
+
+
+# modal 변경 예정
+def login(request):
+    if request.method == 'POST':
+        login_form = AuthenticationForm(request, data=request.POST)
+        if login_form.is_valid():
+            auth_login(request, login_form.get_user())
+            return redirect(request.GET.get('next') or 'articles:index')
+
+    
+    else:
+        login_form = AuthenticationForm()
+
+    context = {
+        'login_form' : login_form
+    }
+
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    
+    auth_logout(request)
+
+    return redirect('articles:index')
