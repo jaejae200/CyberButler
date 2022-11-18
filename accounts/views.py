79d41e49077184f1f4a3108e9dc8bd0d3baff35d
jaejae200 <kj31049@naver.com>
2022-11-18 14:21:25 +0900

    feat : 회원가입 기능

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def signup(request):
+    if request.method == 'POST':
+        signup_form = CustomUserCreationForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
+            login(request, user)
+            return redirect('articles:index')
+    else:
+        signup_form = CustomUserCreationForm()
 
-    return render(request, 'accounts/signup.html')
+    context = {
+        'signup_form' : signup_form, 
+    }
+
+    return render(request, 'accounts/signup.html', context)
