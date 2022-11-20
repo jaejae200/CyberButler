073f67a63c6757f46c6b94ed7237d212a7792b05
jaejae200 <kj31049@naver.com>
2022-11-20 21:10:45 +0900

    feat : 글 작성 기능

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
-# class Article(models.Model):
-#     image = models.ImageField(blank=True, upload_to='images/')
-#     content = models.TextField()
+class Article(models.Model):
+    image = models.ImageField(blank=True, upload_to='images/')
+    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+
     
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import ArticleForm, CommentForm
+
 
 # Create your views here.
 
 def index(request):
+    
     return render(request, 'articles/index.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, request.FILES)
+        if article_form.is_valid():
+            article = article_form.save(commit=False)
+            article.user = request.user 
+            article.save()
+            return redirect('articles:index')
+    else: 
+        article_form = ArticleForm()
+    
+    context = {
+        'article_form': article_form
+    }
+    
+    return render(request, 'articles/form.html', context=context)
